bd669cabca8ee44e57836f22da5d6c975278673b
Praise Yu <praiseyu@gmail.com>
2024-05-31 11:32:14 -0600

    Link questions to question container.

--- src/components/Questionnaire1/Questionnaire1.jsx
+++ src/components/Questionnaire1/Questionnaire1.jsx
@@ -8,29 +8,29 @@ export default function Questionnaire1() {
                 <div className="form__question"></div>
                     <div className="form__input">
                         <input type="checkbox" name="q11" value="q11"/>
-                        <label for="savings">A. Savings</label>
+                        <label htmlFor="savings">A. Savings</label>
                     </div>
                     <div className="form__input">
                         <input type="checkbox" name="q12" value="q12"/>
-                        <label for="savings">B. Chequing</label>
+                        <label htmlFor="savings">B. Chequing</label>
                     </div>
                     <div className="form__input">
                         <input type="checkbox" name="q13" value="q13"/>
-                        <label for="savings">C. TFSA</label>
+                        <label htmlFor="savings">C. TFSA</label>
                     </div>
                     <div className="form__input">
                         <input type="checkbox" name="q14" value="q14"/>
-                        <label for="savings">D. RRSP</label>
+                        <label htmlFor="savings">D. RRSP</label>
                     </div>
                     <div className="form__input">
                         <input type="checkbox" name="q11" value="q11"/>
-                        <label for="savings">E. FHSA</label>
+                        <label htmlFor="savings">E. FHSA</label>
                     </div>
-                <div className="form__button">   
+                {/* <div className="form__button">   
                     <button type="submit">Next</button>
-                </div>
-                <div className="form__bar">
-                </div>
+                </div> */}
+                {/* <div className="form__bar">
+                </div> */}
             </form>
 
 
