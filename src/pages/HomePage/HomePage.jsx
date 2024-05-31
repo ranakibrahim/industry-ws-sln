509b021f4edffd4631037fffef1cf3fdd3e0dafe
Rana Ibrahim <ranakibrahim02@gmail.com>
2024-05-31 13:45:20 -0400

    Fixed merged issues

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import "./HomePage.scss";
 import Headers from "../../components/Headers/Headers";
-import QuestionContainer from "./components/QuestionContainer/QuestionContainer";
+import QuestionContainer from "../../components/QuestionContainer/QuestionContainer";
 
 export default function HomePage() {
   return (
