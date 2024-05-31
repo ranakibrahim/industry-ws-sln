5b884a71cb541843d5d8eec196bcb2a4548506cf
Rana Ibrahim <ranakibrahim02@gmail.com>
2024-05-31 09:48:37 -0400

    Added body headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import NavBar from "./components/NavBar/NavBar";
+import Quiz from "./components/Quiz/Quiz";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Quiz/>
     </>
   );
 }
