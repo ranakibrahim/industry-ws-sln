aa4ad2abe370dd6ae3925de372e71d576d458046
Praise Yu <praiseyu@gmail.com>
2024-05-31 10:24:23 -0600

    Finish progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import NavBar from "./components/NavBar/NavBar";
 import Headers from "./components/Headers/Headers";
+import ProgressBar from "./components/ProgressBar/ProgressBar";
 
 function App() {
   return (
     <>
       <NavBar />
       <Headers />
+      <ProgressBar />
     </>
   );
 }
