acdb067bc627c852e8eb26117a227808e55602b1
Norah <norah618@outlook.com>
2024-05-31 11:52:28 -0400

    Create questionnaire component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.scss";
 import NavBar from "./components/NavBar/NavBar";
 import Headers from "./components/Headers/Headers";
+import Questionnaire from "./components/Questionnaire/Questionnaire";
 
 function App() {
   return (
     <>
       <NavBar />
       <Headers />
+      <main className="main">
+        <Questionnaire />
+      </main>
     </>
   );
 }
---
+++ src/components/Questionnaire/Questionnaire.jsx
@@ -0,0 +1,15 @@
+import "./Questionnaire.scss";
+
+export default function Questionnaire() {
+    return (
+        <section className="outer">
+
+            <p1>Hello world</p1>
+
+
+
+        </section>
+
+
+    )
+}
